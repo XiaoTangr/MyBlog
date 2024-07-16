bc2432ff444453cd571deb2954e350b70e2590fe
XiaoTang <3083649887@qq.com>
2024-07-16 22:37:24 +0800

    reinit Server by spring boot

---
+++ BolgServer/src/main/java/site/icefox/myblog/bolgserver/BolgServerApplication.java
@@ -0,0 +1,13 @@
+package site.icefox.myblog.bolgserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolgServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolgServerApplication.class, args);
+    }
+
+}
