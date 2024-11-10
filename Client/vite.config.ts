2fb0247405d408fee1ae8bab985a22d2ecf37344
XiaoTang <3083649887@qq.com>
2024-11-10 17:04:01 +0800

    add files

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -4,29 +4,6 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//     vueDevTools(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     }
-//   },
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: env.VITE_BASE_API,
-//         ws: true,
-//         secure: false,
-//         changeOrigin: true,// 是否允许跨域代理
-//         rewrite: (path) => path.replace(/^\/api/, '')
-//       }
-//     }
-//   }
-// })
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
